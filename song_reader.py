ceced695d604e63987b3ced1f5b984e646e3dc23
JosieS <sonesjosie@gmail.com>
2025-05-27 13:17:58 -0500

    added song function without led lights

--- song_reader.py
+++ song_reader.py
@@ -1,3 +1,13 @@
 from codrone_edu.drone import *
 drone = Drone()
 
+
+"""function that takes a dictionary with the key:value format of note:duration
+ and plays it along with optionally flashing the drone and controller's LED light"""
+
+
+def play_song_dict(song, RGB_color="off"):
+    for note, duration in song.items():
+        drone.drone_buzzer(note, duration)
+        
+
