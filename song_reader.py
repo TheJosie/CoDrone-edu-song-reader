6972d7a2b873db0723b107794f043f618c0dead4
JosieS <sonesjosie@gmail.com>
2025-05-27 13:54:52 -0500

    Added led lighting

--- song_reader.py
+++ song_reader.py
@@ -3,11 +3,25 @@ drone = Drone()
 
 
 """function that takes a dictionary with the key:value format of note:duration
- and plays it along with optionally flashing the drone and controller's LED light"""
+ and plays it along with optionally flashing the drone and controller's LED light."""
 
 
 def play_song_dict(song, RGB_color="off"):
+    light_toggle = True
     for note, duration in song.items():
         drone.drone_buzzer(note, duration)
-        
+        # flashs on and off with the rhythm if an rgb color is given
+        if RGB_color != "off":
+            if light_toggle = True:
+                drone.set_drone_LED(RGB_color)
+                drone.set_controller_LED(RGB_color)
+                light_toggle = False
+            else:
+                drone.set_drone_LED("off")
+                drone.set_controller_LED("off")
+                light_toggle = True
+                
+            
+
+
 
