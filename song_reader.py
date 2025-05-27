d090d74d335e7f02479b2690b3a9816a76e147d5
JosieS <sonesjosie@gmail.com>
2025-05-27 14:45:47 -0500

    changed system from dictionary to list with tuples so notes can repeat; added delay to led lights sync

--- song_reader.py
+++ song_reader.py
@@ -1,25 +1,35 @@
+import time
 from codrone_edu.drone import *
 drone = Drone()
 
 
-"""function that takes a dictionary with the key:value format of note:duration
- and plays it along with optionally flashing the drone and controller's LED light."""
+"""put in a list of tuples with the notes and duration
+Ex.
+my_song = {
+    (Note.X, duration),
+    (Note.Y, duration),
+    (Note.Z, duration),
+}"""
 
 
-def play_song_dict(song, RGB_color="off"):
+def play_song_list(song, RGB_color="off"):
     light_toggle = True
-    for note, duration in song.items():
+    for note, duration in song:
         drone.drone_buzzer(note, duration)
         # flashs on and off with the rhythm if an rgb color is given
         if RGB_color != "off":
-            if light_toggle = True:
-                drone.set_drone_LED(RGB_color)
-                drone.set_controller_LED(RGB_color)
-                light_toggle = False
-            else:
-                drone.set_drone_LED("off")
-                drone.set_controller_LED("off")
-                light_toggle = True
+            drone.set_drone_LED(RGB_color if toggle else "off")
+            drone.set_controller_LED(RGB_color if toggle else "off")
+            toggle = not toggle
+        if note == Note.REST:
+            time.sleep(duration / 1000.0)  # pause for rest duration
+        else:
+            drone.drone_buzzer(note, duration)
+            time.sleep(duration / 1000.0)  # wait so LED flashing syncs
+    drone.set_drone_LED("off")
+    drone.set_controller_LED("off")
+
+
                 
             
 
